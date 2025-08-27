a0fea2f858c0fb5607c1e5db34f4f8f13e06eea1
lwx <lwx@macbookair>
2025-08-27 15:44:20 +0800

    added some methods

--- z.go
+++ z.go
@@ -1,9 +1,47 @@
 package z
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type App struct {
 	mux *http.ServeMux
+}
+
+type Z struct {
+	rw http.ResponseWriter
+	r  *http.Request
+}
+
+func (z *Z) String(respStr string) {
+	z.rw.Write([]byte(respStr))
+}
+
+func (z *Z) JSON(respJSON any) {
+	z.rw.Header().Set("content-type", "application/json")
+	json.NewEncoder(z.rw).Encode(respJSON)
+}
+
+func (z *Z) BindBody(reqBodyType any) error {
+	if z.r.Body == nil {
+		return fmt.Errorf("request body is nil")
+	}
+
+	defer z.r.Body.Close()
+	return json.NewDecoder(z.r.Body).Decode(reqBodyType)
+}
+
+func (app *App) GET(path string, handler func(z *Z)) {
+	app.mux.HandleFunc(fmt.Sprintf("%s %s", http.MethodGet, path), func(w http.ResponseWriter, r *http.Request) {
+		zHandler := &Z{
+			rw: w,
+			r:  r,
+		}
+
+		handler(zHandler)
+	})
 }
 
 func New() *App {

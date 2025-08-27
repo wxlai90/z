acf93ad5f11dbbddf4732db06ef04c778daddb73
lwx <lwx@macbookair>
2025-08-27 15:53:12 +0800

    added write status code

--- z.go
+++ z.go
@@ -17,10 +17,12 @@ type Z struct {
 }
 
 func (z *Z) String(statusCode int, respStr string) {
+	z.rw.WriteHeader(statusCode)
 	z.rw.Write([]byte(respStr))
 }
 
 func (z *Z) JSON(statusCode int, respJSON any) {
+	z.rw.WriteHeader(statusCode)
 	z.rw.Header().Set("content-type", "application/json")
 	json.NewEncoder(z.rw).Encode(respJSON)
 }
